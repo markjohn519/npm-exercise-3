e5e5c3a2cda35f75bb55c25d17e0740dbcdce566
markjohn519 <markjohnmonton@gmail.com>
2022-10-11 09:18:51 +0800

    modified the index using prettier

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const [ { parse, toString, toArray }, { argv } ] = [ require('@estilles/expression-parser'), require('node:process') ];
+const [{ parse, toString, toArray }, { argv }] = [
+  require('@estilles/expression-parser'),
+  require('node:process'),
+];
 
-const [,,args] = process.argv;
+const [, , args] = process.argv;
 
-if (args == 1) {
-  console.log(args);
+if (args !== 1) {
   console.log('Only one argument is allowed');
   process.exit();
 }
 
 const expressionParser = (args) => {
-  let string  = toString(parse(args));
+  let string = toString(parse(args));
   let array = toArray(parse(args));
 
-  return {string, array};
-}
+  return { string, array };
+};
 
 console.log(expressionParser(args));
